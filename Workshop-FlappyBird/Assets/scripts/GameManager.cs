4ccb2d843f874f0e0b7bd9b734c41ae673d45e74
Magnus Geert <magnusgeert@gmail.com>
2024-04-05 13:04:08 +0200

    connects gamemanager to player script

--- Workshop-FlappyBird/Assets/scripts/GameManager.cs
+++ Workshop-FlappyBird/Assets/scripts/GameManager.cs
@@ -6,29 +6,31 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
 	private bool gameStarted = false;
-
+	public static int currentScene = 1;
+// private SceneManager sceneManager;
 	// Start is called before the first frame update
 	void Start()
 	{
-
+  //  sceneManager = gameObject.GetComponent<SceneManager>();
 	}
 
-	Scene currentScene = SceneManager.getActiveScene();
+
 	public static int score = 0; // Static var to hold the score
 
 	// Update is called once per frame
 	void Update()
 	{
-		if (currentScene = 1)
+		if (currentScene == 1)
 		{
 			gameStarted = true;
-			scoreText.text = "Score: " + score;
+			//scoreText.text = "Score: " + score;
 
 		}
 	}
 
 	public void EndGame()
 	{
+		Debug.Log("Game Over");
 		SceneManager.LoadScene(2);
 	}
 
