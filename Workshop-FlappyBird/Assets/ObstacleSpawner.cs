b4db71b08eb2da3affbb29453bc61d37db9870ef
olakupczyk <10407738@ucn.dk>
2024-04-05 12:30:40 +0200

    Created Obstacle Spawner

---
+++ Workshop-FlappyBird/Assets/ObstacleSpawner.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+
+    public GameObject Obstacle;
+
+    public float spawnInterval = 2f; // Time between each obstacle spawn
+    public float moveSpeed = 2f; // Speed at which obstacles move to the left
+    public float destroyOffset = 2f; // Offset outside the visible part of the camera view to destroy obstacles
+
+    private float timer; // Timer for obstacle spawning
+
+    // Start is called before the first frame update
+
+
+    public void SpawnObstacle()
+    {
+        // Instantiate the obstacle prefab
+        GameObject newObstacle = Instantiate(Obstacle, transform.position, Quaternion.identity);
+
+        // Move the obstacle to the left of the camera view
+        newObstacle.transform.position = new Vector3(Camera.main.transform.position.x + Camera.main.orthographicSize * Camera.main.aspect + destroyOffset,
+                                                      Random.Range(-2f, 2f), 0f);
+    }
+
+
+    public void MoveObstacles()
+    {
+
+        GameObject[] obstacles = GameObject.FindGameObjectsWithTag("ObstacleMain");
+        foreach (GameObject obstacle in obstacles)
+        {
+            obstacle.transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+
+            if (obstacle.transform.position.x < Camera.main.transform.position.x - Camera.main.orthographicSize * Camera.main.aspect - destroyOffset)
+            {
+                // Destroy obstacle
+                Destroy(obstacle);
+            }
+        }
+
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Increment timer
+        timer += Time.deltaTime;
+
+        // If it's time to spawn a new obstacle
+        if (timer >= spawnInterval)
+        {
+            SpawnObstacle();
+            timer = 0f; // Reset timer
+        }
+
+        // Move existing obstacles to the left
+        MoveObstacles();
+
+    }
+}
+
